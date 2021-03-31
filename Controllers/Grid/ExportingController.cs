41310ef8bec774ece3053ee2240fa91571281dfd
buildautomation <buildautomation@syncfusion.com>
2021-03-31 17:15:44 +0530

    v19.1.0.54

---
+++ Controllers/Grid/ExportingController.cs
@@ -0,0 +1,76 @@
+#region Copyright Syncfusion Inc. 2001-2021.
+// Copyright Syncfusion Inc. 2001-2021. All rights reserved.
+// Use of this code is subject to the terms of our license.
+// A copy of the current license can be obtained at any time by e-mailing
+// licensing@syncfusion.com. Any infringement will be prosecuted under
+// applicable laws. 
+#endregion
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using samplebrowser.Models;
+using Syncfusion.EJ.Export;
+using Syncfusion.JavaScript.Models;
+using Newtonsoft.Json;
+using System.Collections;
+using Syncfusion.XlsIO;
+
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace samplebrowser.Controllers.Grid
+{
+    public partial class GridController : Controller
+    {
+
+        private NORTHWNDContext _context;
+
+        public GridController(NORTHWNDContext context)
+        {
+            _context = context;
+        }
+        // GET: /<controller>/
+        public ActionResult Exporting()
+        {
+            ViewBag.datasource = _context.Orders.Take(100).ToList();
+            return View();
+        }
+        public ActionResult ExportToExcel(string GridModel)
+        {
+            ExcelExport exp = new ExcelExport();
+            var DataSource = _context.Orders.Take(100).ToList();
+            GridProperties gridProp = ConvertGridObject(GridModel);
+            GridExcelExport excelExp = new GridExcelExport();
+            excelExp.FileName = "Export.xlsx"; excelExp.Excelversion = ExcelVersion.Excel2010;
+            excelExp.Theme = "flat-saffron";
+            return exp.Export(gridProp, DataSource, excelExp);
+        }
+
+        public ActionResult ExportToWord(string GridModel)
+        {
+            WordExport exp = new WordExport();
+            var DataSource = _context.Orders.Take(100).ToList();
+            GridProperties gridProp = ConvertGridObject(GridModel);
+            GridWordExport wrdExp = new GridWordExport();
+            wrdExp.FileName = "Export.docx"; wrdExp.Theme = "flat-saffron";
+            return exp.Export(gridProp, DataSource, wrdExp);
+        }
+        public ActionResult ExportToPdf(string GridModel)
+        {
+            PdfExport exp = new PdfExport();
+            var DataSource = _context.Orders.Take(100).ToList();
+            GridProperties gridProp = ConvertGridObject(GridModel);
+            GridPdfExport pdfExp = new GridPdfExport();
+            pdfExp.FileName = "Export.pdf"; pdfExp.Theme = "flat-saffron";
+            return exp.Export(gridProp, DataSource, pdfExp);
+        }
+        private GridProperties ConvertGridObject(string gridProperty)
+        {
+            GridProperties gridProp = new GridProperties();
+            gridProp = (GridProperties)JsonConvert.DeserializeObject(gridProperty, typeof(GridProperties));
+            return gridProp;
+        }
+    }
+}
---
+++ bin/Controllers/Chart/AreaController.cs
@@ -0,0 +1,26 @@
+#region Copyright Syncfusion Inc. 2001-2021.
+// Copyright Syncfusion Inc. 2001-2021. All rights reserved.
+// Use of this code is subject to the terms of our license.
+// A copy of the current license can be obtained at any time by e-mailing
+// licensing@syncfusion.com. Any infringement will be prosecuted under
+// applicable laws. 
+#endregion
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace samplebrowser.Controllers
+{
+    public partial class ChartController : Controller
+    {
+        //
+        // GET: /Chart/
+
+        public ActionResult Area()
+        {
+            return View();
+        }
+
+    }
+}
