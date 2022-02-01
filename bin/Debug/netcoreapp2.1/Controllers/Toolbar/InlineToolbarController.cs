f06a45b06d31fe36eca6c8178eb8320c259198f0
SyncfusionBuild <buildautomation@syncfusion.com>
2022-02-01 10:33:53 +0530

    v19.4.0.48

---
+++ bin/Debug/netcoreapp2.1/Controllers/Toolbar/InlineToolbarController.cs
@@ -0,0 +1,53 @@
+#region Copyright Syncfusion Inc. 2001-2022.
+// Copyright Syncfusion Inc. 2001-2022. All rights reserved.
+// Use of this code is subject to the terms of our license.
+// A copy of the current license can be obtained at any time by e-mailing
+// licensing@syncfusion.com. Any infringement will be prosecuted under
+// applicable laws. 
+#endregion
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace samplebrowser.Controllers
+{
+    public partial class ToolbarController : Controller
+    {
+        // GET: InlineToolbar
+        List<InlineToolbar> I = new List<InlineToolbar>();
+        public ActionResult InlineToolbar()
+        {
+            I.Add(new InlineToolbar { id = "1", spriteCss = "e-icon e-cut_01", group = "g1", tooltiptext="Cut"});
+            I.Add(new InlineToolbar { id = "2", spriteCss = "e-icon e-copy_02", group = "g1",tooltiptext="Copy" });
+            I.Add(new InlineToolbar { id = "3", spriteCss = "e-icon e-paste_01", group = "g1" ,tooltiptext="Paste"});
+            I.Add(new InlineToolbar { id = "4", spriteCss = "e-icon e-edit", group = "g1" ,tooltiptext="Edit"});
+            I.Add(new InlineToolbar { id = "5", spriteCss = "e-icon e-save", group = "g1",tooltiptext="Save" });
+            I.Add(new InlineToolbar { id = "6", spriteCss = "e-icon e-bold_01", group = "g2" ,tooltiptext="Bold"});
+            I.Add(new InlineToolbar { id = "7", spriteCss = "e-icon e-underline_01", group = "g2" ,tooltiptext="Underline"});
+            I.Add(new InlineToolbar { id = "8", spriteCss = "e-icon  e-strikethrough_01", group = "g2" ,tooltiptext="strikethrough"});
+            I.Add(new InlineToolbar { id = "9", spriteCss = "e-icon e-align-left_01", group = "g3" ,tooltiptext="Align Left"});
+            I.Add(new InlineToolbar { id = "10", spriteCss = "e-icon e-align-center_01", group = "g3" ,tooltiptext="Align Center"});
+            I.Add(new InlineToolbar { id = "11", spriteCss = "e-icon e-align-right_01", group = "g3" ,tooltiptext="Align Right" });
+            I.Add(new InlineToolbar { id = "12", spriteCss = "e-icon  e-align-justify_01", group = "g3",tooltiptext="Align Justify"});
+            I.Add(new InlineToolbar { id = "13", spriteCss = "e-icon  e-reload", group = "g4" ,tooltiptext="Reload"});
+            I.Add(new InlineToolbar { id = "14", spriteCss = "e-icon e-delete", group = "g4",tooltiptext="delete" });
+            I.Add(new InlineToolbar { id = "15", spriteCss = "e-icon e-print_02", group = "g4", tooltiptext = "Print" });
+            I.Add(new InlineToolbar { id = "16", spriteCss = "e-icon e-excel-export", group = "g4" ,tooltiptext="Export"});
+            ViewBag.datasource = I;
+            return View();
+        }
+    }
+    public class InlineToolbar
+    {
+        public string id { get; set; }
+        public string spriteCss { get; set; }
+        public string group { get; set; }
+
+        public string tooltiptext { get; set; }
+
+    }
+}
---
+++ wwwroot/scripts/Gantt/GanttData.js
@@ -0,0 +1,168 @@
+﻿var dayData = [
+          {
+              "TaskID": 1,
+              "TaskName": "Parent Task 1",
+              "StartDate": "02/23/2014",
+              "duration":7,
+              "Progress": "40",
+              "Children": [
+                   { "TaskID": 2, "TaskName": "Child Task 1", "StartDate": "02/23/2014 08:00:00 AM", "duration": 7, "Progress": "40" },
+                   { "TaskID": 3, "TaskName": "Child Task 2", "StartDate": "02/23/2014 08:00:00 AM", "duration": 7, "Progress": "40", },
+                   { "TaskID": 4, "TaskName": "Child Task 3", "StartDate": "02/23/2014 08:00:00 AM", "duration": 7, "Progress": "40", }
+              ]
+          },
+          {
+              "TaskID": 5,
+              "TaskName": "Parent Task 2",
+              "StartDate": "02/24/2014",
+              "duration": 7,
+              "Progress": "40",
+              "Children": [
+                   { "TaskID": 6, "TaskName": "Child Task 1", "StartDate": "02/23/2014 10:00:00 PM", "duration": 7, "Progress": "40" },
+                   { "TaskID": 7, "TaskName": "Child Task 2", "StartDate": "02/23/2014 10:00:00 PM", "duration": 7, "Progress": "40", },
+                   { "TaskID": 8, "TaskName": "Child Task 3", "StartDate": "02/23/2014 10:00:00 PM", "duration": 7, "Progress": "40", },
+                   { "TaskID": 9, "TaskName": "Child Task 4", "StartDate": "02/23/2014 10:00:00 PM", "duration": 7, "Progress": "40" }
+              ]
+          },
+          {
+              "TaskID": 10,
+              "TaskName": "Parent Task 3",
+              "StartDate": "02/25/2014",
+              "duration": 7,
+              "Progress": "40",
+              "Children": [
+                   { "TaskID": 11, "TaskName": "Child Task 1", "StartDate": "02/24/2014 12:00:00 PM", "duration": 7, "Progress": "40" },
+                   { "TaskID": 12, "TaskName": "Child Task 2", "StartDate": "02/24/2014 12:00:00 PM", "duration": 7, "Progress": "40", },
+                   { "TaskID": 13, "TaskName": "Child Task 3", "StartDate": "02/24/2014 12:00:00 PM", "duration": 7, "Progress": "40", },
+                   { "TaskID": 14, "TaskName": "Child Task 4", "StartDate": "02/24/2014 12:00:00 PM", "duration": 7, "Progress": "40", },
+                   { "TaskID": 15, "TaskName": "Child Task 5", "StartDate": "02/24/2014 12:00:00 PM", "duration": 7, "Progress": "40", }
+              ]
+          }
+
+];
+var hourData = [
+          {
+              "TaskID": 1,
+              "TaskName": "Parent Task 1",
+              "StartDate": "02/23/2014",
+              "duration": 40,
+              "Progress": "40",
+              "Children": [
+                   { "TaskID": 2, "TaskName": "Child Task 1", "StartDate": "02/23/2014 01:00:00 AM", "duration": 40, "Progress": "40" },
+                   { "TaskID": 3, "TaskName": "Child Task 2", "StartDate": "02/23/2014 01:00:00 AM", "duration": 40, "Progress": "40", },
+                   { "TaskID": 4, "TaskName": "Child Task 3", "StartDate": "02/23/2014 01:00:00 AM", "duration": 40, "Progress": "40", }
+              ]
+          },
+          {
+              "TaskID": 5,
+              "TaskName": "Parent Task 2",
+              "StartDate": "02/23/2014",
+              "duration": 40,
+              "Progress": "40",
+              "Children": [
+                   { "TaskID": 6, "TaskName": "Child Task 1", "StartDate": "02/23/2014 02:00:00 AM", "duration": 40, "Progress": "40" },
+                   { "TaskID": 7, "TaskName": "Child Task 2", "StartDate": "02/23/2014 02:00:00 AM", "duration": 40, "Progress": "40", },
+                   { "TaskID": 8, "TaskName": "Child Task 3", "StartDate": "02/23/2014 02:00:00 AM", "duration": 40, "Progress": "40", },
+                   { "TaskID": 9, "TaskName": "Child Task 4", "StartDate": "02/23/2014 02:00:00 AM", "duration": 40, "Progress": "40" }
+              ]
+          },
+          {
+              "TaskID": 10,
+              "TaskName": "Parent Task 3",
+              "StartDate": "02/23/2014",
+              "duration": 40,
+              "Progress": "40",
+              "Children": [
+                   { "TaskID": 11, "TaskName": "Child Task 1", "StartDate": "02/23/2014 03:00:00 AM", "duration": 40, "Progress": "40" },
+                   { "TaskID": 12, "TaskName": "Child Task 2", "StartDate": "02/23/2014 03:00:00 AM", "duration": 40, "Progress": "40", },
+                   { "TaskID": 13, "TaskName": "Child Task 3", "StartDate": "02/23/2014 03:00:00 AM", "duration": 40, "Progress": "40", },
+                   { "TaskID": 14, "TaskName": "Child Task 4", "StartDate": "02/23/2014 03:00:00 AM", "duration": 40, "Progress": "40", },
+                   { "TaskID": 15, "TaskName": "Child Task 5", "StartDate": "02/23/2014 03:00:00 AM", "duration": 40, "Progress": "40", }
+              ]
+          }
+
+];
+var weekData = [
+     {
+         "TaskID": 1,
+         "TaskName": "Parent Task 1",
+         "StartDate": "02/23/2014",
+         "EndDate": "02/28/2014",
+         "Progress": "40",
+         "Children": [
+              { "TaskID": 2, "TaskName": "Child Task 1", "StartDate": "02/23/2014", "EndDate": "02/28/2014", "Progress": "40" },
+              { "TaskID": 3, "TaskName": "Child Task 2", "StartDate": "02/23/2014", "EndDate": "02/28/2014", "Progress": "40", },
+              { "TaskID": 4, "TaskName": "Child Task 3", "StartDate": "02/23/2014", "EndDate": "02/28/2014", "Progress": "40", }
+         ]
+     },
+     {
+         "TaskID": 5,
+         "TaskName": "Parent Task 2",
+         "StartDate": "03/03/2014",
+         "EndDate": "03/07/2014",
+         "Progress": "40",
+         "Children": [
+              { "TaskID": 6, "TaskName": "Child Task 1", "StartDate": "03/03/2014", "EndDate": "03/07/2014", "Progress": "40" },
+              { "TaskID": 7, "TaskName": "Child Task 2", "StartDate": "03/03/2014", "EndDate": "03/07/2014", "Progress": "40", },
+              { "TaskID": 8, "TaskName": "Child Task 3", "StartDate": "03/03/2014", "EndDate": "03/07/2014", "Progress": "40", },
+              { "TaskID": 9, "TaskName": "Child Task 4", "StartDate": "03/03/2014", "EndDate": "03/07/2014", "Progress": "40" }
+         ]
+     },
+     {
+         "TaskID": 10,
+         "TaskName": "Parent Task 3",
+         "StartDate": "03/10/2014",
+         "EndDate": "03/14/2014",
+         "Progress": "40",
+         "Children": [
+              { "TaskID": 11, "TaskName": "Child Task 1", "StartDate": "03/10/2014", "EndDate": "03/14/2014", "Progress": "40" },
+              { "TaskID": 12, "TaskName": "Child Task 2", "StartDate": "03/10/2014", "EndDate": "03/14/2014", "Progress": "40", },
+              { "TaskID": 13, "TaskName": "Child Task 3", "StartDate": "03/10/2014", "EndDate": "03/14/2014", "Progress": "40", },
+              { "TaskID": 14, "TaskName": "Child Task 4", "StartDate": "03/10/2014", "EndDate": "03/14/2014", "Progress": "40", },
+              { "TaskID": 15, "TaskName": "Child Task 5", "StartDate": "03/10/2014", "EndDate": "03/14/2014", "Progress": "40", }
+         ]
+     }
+
+];
+var monthData = [
+     {
+         "TaskID": 1,
+         "TaskName": "Parent Task 1",
+         "StartDate": "02/23/2014",
+         "EndDate": "04/28/2014",
+         "Progress": "40",
+         "Children": [
+              { "TaskID": 2, "TaskName": "Child Task 1", "StartDate": "02/23/2014", "EndDate": "03/28/2014", "Progress": "40" },
+              { "TaskID": 3, "TaskName": "Child Task 2", "StartDate": "02/23/2014", "EndDate": "03/28/2014", "Progress": "40", },
+              { "TaskID": 4, "TaskName": "Child Task 3", "StartDate": "02/23/2014", "EndDate": "03/28/2014", "Progress": "40", }
+         ]
+     },
+     {
+         "TaskID": 5,
+         "TaskName": "Parent Task 2",
+         "StartDate": "05/03/2014",
+         "EndDate": "07/07/2014",
+         "Progress": "40",
+         "Children": [
+              { "TaskID": 6, "TaskName": "Child Task 1", "StartDate": "04/13/2014", "EndDate": "05/20/2014", "Progress": "40" },
+              { "TaskID": 7, "TaskName": "Child Task 2", "StartDate": "04/13/2014", "EndDate": "05/20/2014", "Progress": "40", },
+              { "TaskID": 8, "TaskName": "Child Task 3", "StartDate": "04/13/2014", "EndDate": "05/20/2014", "Progress": "40", },
+              { "TaskID": 9, "TaskName": "Child Task 4", "StartDate": "04/13/2014", "EndDate": "05/20/2014", "Progress": "40" }
+         ]
+     },
+     {
+         "TaskID": 10,
+         "TaskName": "Parent Task 3",
+         "StartDate": "08/10/2014",
+         "EndDate": "10/14/2014",
+         "Progress": "40",
+         "Children": [
+              { "TaskID": 11, "TaskName": "Child Task 1", "StartDate": "06/10/2014", "EndDate": "07/14/2014", "Progress": "40" },
+              { "TaskID": 12, "TaskName": "Child Task 2", "StartDate": "06/10/2014", "EndDate": "07/14/2014", "Progress": "40", },
+              { "TaskID": 13, "TaskName": "Child Task 3", "StartDate": "06/10/2014", "EndDate": "07/14/2014", "Progress": "40", },
+              { "TaskID": 14, "TaskName": "Child Task 4", "StartDate": "06/10/2014", "EndDate": "07/14/2014", "Progress": "40", },
+              { "TaskID": 15, "TaskName": "Child Task 5", "StartDate": "06/10/2014", "EndDate": "07/14/2014", "Progress": "40", }
+         ]
+     }
+
+];
